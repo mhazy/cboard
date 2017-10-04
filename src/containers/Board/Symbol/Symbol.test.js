b52ed21d95a1d520b678a44664dd767aac8c33ab
Shay Cojocaru <shayc@outlook.com>
2017-10-04 20:02:24 +0300

    fix symbol tests

--- src/containers/Board/Symbol/Symbol.test.js
+++ src/containers/Board/Symbol/Symbol.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Symbol } from './Symbol';
-
-const intl = { formatMessage: () => {} };
+import Symbol from './Symbol';
 
 it('renders without crashing', () => {
   shallow(<Symbol />);
+});
+
+it('renders with .Symbol__image', () => {
+  const symbolSrc = 'path/to/img.svg';
+  const wrapper = shallow(<Symbol src={symbolSrc} />);
+  expect(wrapper.find('.Symbol__image')).toHaveLength(1);
+});
+
+it('renders with correct image source path', () => {
+  const symbolSrc = 'path/to/img.svg';
+  const wrapper = shallow(<Symbol src={symbolSrc} />);
+  const symbolImage = wrapper.find('.Symbol__image');
+  expect(symbolImage.prop('src')).toEqual(symbolSrc);
+});
+
+it('renders with .Symbol__label', () => {
+  const wrapper = shallow(<Symbol label="dummy label" />);
+  expect(wrapper.find('.Symbol__label')).toHaveLength(1);
 });
